836f70be79702809ca07b1955dc2d3f4b74ddc8f
Manuel Scholz <manuel.serrano943@gmail.com>
2023-01-25 15:55:45 +0100

    Feat: Mejorar animación para cambiar de frase

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { Gesture, GestureDetector, GestureHandlerRootView, ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import { useAnimatedStyle, useSharedValue, withTiming, runOnJS, Easing } from 'react-native-reanimated';
+import Animated from 'react-native-reanimated';
 import { supabase } from "./src/supabaseClient"
 
 export default function App() {
@@ -14,6 +16,8 @@ export default function App() {
     // Longitud de la base de datos
     let dbLength = useRef(0);
 
+    const position = useSharedValue(0);
+
     useEffect(() => {
         getPhrasesLength();
         getPhrases();
@@ -23,86 +27,79 @@ export default function App() {
         // Cada 3 frases leidas, carga otras 4 en el array
         if (arr.length > 0) {
             if (phrasesReaded === arrLength - 1) {
-                console.log("3 FRASES LEIDAS, ACTUALIZO ARR");
                 getPhrases();
             }
         }
     }, [phrasesReaded])
 
     async function getPhrasesLength() {
-        const { data, count } = 
-        await supabase
-        .from("test")
-        .select('*', { count: 'exact' })
+        const { data, count } =
+            await supabase
+                .from("test")
+                .select('*', { count: 'exact' })
 
         dbLength.current = count;
     }
-    
-    async function getPhrases() {
-        console.log("Get phrases.");
-        await supabase
-        .from('test')
-        .select("phrase")
-        .range(arrLength, arrLength+3)
-        .then((res) => {
-            if (arr.length < 1) {
-                setArr(res.data);
-            } else {
-                setArr(arr.concat(res.data))
-            }
 
-        })
+    async function getPhrases() {
+        await supabase
+            .from('test')
+            .select("phrase")
+            .range(arrLength, arrLength + 3)
+            .then((res) => {
+                if (arr.length < 1) {
+                    setArr(res.data);
+                } else {
+                    setArr(arr.concat(res.data))
+                }
+
+            })
         setArrLength(() => arrLength + 4);
     }
 
-    const tap = Gesture.Pan()
-    .activeOffsetY([-90, 90])
-    .onEnd((e) => {
-        if (e.translationY < 0) {
-            if (phrasesReaded > 0) {
-                setPhrasesReaded(() => phrasesReaded - 1);
-            }
-        } else {
-            if (phrasesReaded < dbLength.current - 1) {
-                setPhrasesReaded(() => phrasesReaded + 1);
-            }
-        }
-        
-        console.log(arr);
+    const tap = Gesture.Pan().runOnJS(true)
+        .activeOffsetY([-100, 100])
+        .onUpdate((e) => {
+            console.log(e.translationY);
+            console.log("ola?");
+            position.value = e.translationY;
+        })
+        .onEnd((e) => {
+            position.value = withTiming(position.value * 10, { duration: 300, easing: Easing.ease });
+            setTimeout(() => {
+                if (e.translationY < 0) {
+                    if (phrasesReaded > 0) {
+                        setPhrasesReaded(() => phrasesReaded - 1);
+                    }
+                } else {
+                    if (phrasesReaded < dbLength.current - 1) {
+                        setPhrasesReaded(() => phrasesReaded + 1);
+                    }
+                }
+                position.value = 0
+            }, 350)
+        })
 
-    });
+
+    const animatedStyle = useAnimatedStyle(() => ({
+        transform: [{ translateY: position.value }],
+    }));
 
     return (
-        <GestureHandlerRootView style={{flex: 1}}>
-            <View style={{flex: 1, marginTop: 50, backgroundColor: "red"}}>
-                <GestureDetector style={{backgroundColor: "black"}} gesture={tap}>
-                    <View style={{flex: 1, backgroundColor: "green"}} collapsable={false}>
-                        <Text>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <GestureDetector style={{ backgroundColor: "black" }} gesture={tap}>
+                <View style={{ flex: 1, marginTop: 50, justifyContent: "center", alignItems: "center" }}>
+                    <Animated.View style={[animatedStyle]} collapsable={false}>
+                        <Text style={{ width: 200, textAlign: "center" }}>
                             {
                                 arr[phrasesReaded] && arr[phrasesReaded].phrase
                             }
                         </Text>
-                    </View>
-                </GestureDetector>
-            </View>
+                    </Animated.View>
+                </View>
+            </GestureDetector>
         </GestureHandlerRootView>
-        
+
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        zIndex: 1,
-        backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    dragHandler: {
-        alignSelf: 'stretch',
-        height: 64,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#ccc'
-    }
-});
