6c60f44e188d01093c708f4574954b42f07f5d45
Manuel Scholz <manuel.serrano943@gmail.com>
2023-01-25 11:29:15 +0100

    Feat: Funcionalidad básica para hacer sliding una serie de frases, recargar array con 4 frases nuevas tras haber leido 3 y así consecutivamente, control de ultima frase e indice de la frase menor a 0

--- App.js
+++ App.js
@@ -1,20 +1,108 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { Gesture, GestureDetector, GestureHandlerRootView, ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import { supabase } from "./src/supabaseClient"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    // Array de frases.
+    const [arr, setArr] = useState([]);
+    // Cantidad de elementos del array.
+    const [arrLength, setArrLength] = useState(0);
+    // Cantidad de frases leídas, también sirve como el índice del array para renderizar la frase
+    const [phrasesReaded, setPhrasesReaded] = useState(0);
+    // Longitud de la base de datos
+    let dbLength = useRef(0);
+
+    useEffect(() => {
+        getPhrasesLength();
+        getPhrases();
+    }, [])
+
+    useEffect(() => {
+        // Cada 3 frases leidas, carga otras 4 en el array
+        if (arr.length > 0) {
+            if (phrasesReaded === arrLength - 1) {
+                console.log("3 FRASES LEIDAS, ACTUALIZO ARR");
+                getPhrases();
+            }
+        }
+    }, [phrasesReaded])
+
+    async function getPhrasesLength() {
+        const { data, count } = 
+        await supabase
+        .from("test")
+        .select('*', { count: 'exact' })
+
+        dbLength.current = count;
+    }
+    
+    async function getPhrases() {
+        console.log("Get phrases.");
+        await supabase
+        .from('test')
+        .select("phrase")
+        .range(arrLength, arrLength+3)
+        .then((res) => {
+            if (arr.length < 1) {
+                setArr(res.data);
+            } else {
+                setArr(arr.concat(res.data))
+            }
+
+        })
+        setArrLength(() => arrLength + 4);
+    }
+
+    const tap = Gesture.Pan()
+    .activeOffsetY([-90, 90])
+    .onEnd((e) => {
+        if (e.translationY < 0) {
+            if (phrasesReaded > 0) {
+                setPhrasesReaded(() => phrasesReaded - 1);
+            }
+        } else {
+            if (phrasesReaded < dbLength.current - 1) {
+                setPhrasesReaded(() => phrasesReaded + 1);
+            }
+        }
+        
+        console.log(arr);
+
+    });
+
+    return (
+        <GestureHandlerRootView style={{flex: 1}}>
+            <View style={{flex: 1, marginTop: 50, backgroundColor: "red"}}>
+                <GestureDetector style={{backgroundColor: "black"}} gesture={tap}>
+                    <View style={{flex: 1, backgroundColor: "green"}} collapsable={false}>
+                        <Text>
+                            {
+                                arr[phrasesReaded] && arr[phrasesReaded].phrase
+                            }
+                        </Text>
+                    </View>
+                </GestureDetector>
+            </View>
+        </GestureHandlerRootView>
+        
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        zIndex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    dragHandler: {
+        alignSelf: 'stretch',
+        height: 64,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#ccc'
+    }
 });
---
+++ src/supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+import 'react-native-url-polyfill/auto';
+
+const supabaseUrl = "https://qebnmxnfniqfbjrbkwpx.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlYm5teG5mbmlxZmJqcmJrd3B4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ2MjczNjMsImV4cCI6MTk5MDIwMzM2M30.msNmZJ4EBF1vjHb2aDfIIhZKtRBKiAGiiqrGNaKumTE";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
