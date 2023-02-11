617f6e70419758f3f03586ac2beec807fa01070a
Manuel Scholz <manuel.serrano943@gmail.com>
2023-02-11 18:42:26 +0100

    Feat: Terminar animación de texto y eliminar librerias que no se usarán en las primeras versiones

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { useEffect, useRef, useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Gesture, GestureDetector, GestureHandlerRootView, ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
-import { useAnimatedStyle, useSharedValue, withTiming, runOnJS, Easing } from 'react-native-reanimated';
+import { useAnimatedStyle, useSharedValue, withTiming, runOnJS, Easing, withDelay } from 'react-native-reanimated';
 import Animated from 'react-native-reanimated';
 import { supabase } from "./src/supabaseClient"
+import {Dimensions} from 'react-native';
 
 export default function App() {
 
@@ -15,7 +16,7 @@ export default function App() {
     const [phrasesReaded, setPhrasesReaded] = useState(0);
     // Longitud de la base de datos
     let dbLength = useRef(0);
-
+    // Posición del elemento para animarlo
     const position = useSharedValue(0);
 
     useEffect(() => {
@@ -31,6 +32,8 @@ export default function App() {
             }
         }
     }, [phrasesReaded])
+
+
 
     async function getPhrasesLength() {
         const { data, count } =
@@ -58,26 +61,30 @@ export default function App() {
     }
 
     const tap = Gesture.Pan().runOnJS(true)
-        .activeOffsetY([-100, 100])
+        .activeOffsetY([60, 60])
         .onUpdate((e) => {
-            console.log(e.translationY);
-            console.log("ola?");
             position.value = e.translationY;
         })
         .onEnd((e) => {
-            position.value = withTiming(position.value * 10, { duration: 300, easing: Easing.ease });
+            position.value = withTiming(position.value * 10, { duration: 400, easing: Easing.ease });
+            if (e.translationY < 60 && e.translationY > -60) {
+                position.value = withTiming(0, { duration: 400, easing: Easing.ease });
+            }
             setTimeout(() => {
-                if (e.translationY < 0) {
+                if (e.translationY > 60) {
                     if (phrasesReaded > 0) {
                         setPhrasesReaded(() => phrasesReaded - 1);
                     }
-                } else {
+                    position.value = -Dimensions.get("window").height;
+                    position.value = withDelay(25, withTiming(0, { duration: 300, easing: Easing.ease }))
+                } else if (e.translationY < -60) {
                     if (phrasesReaded < dbLength.current - 1) {
                         setPhrasesReaded(() => phrasesReaded + 1);
                     }
+                    position.value = Dimensions.get("window").height;
+                    position.value = withDelay(25, withTiming(0, { duration: 300, easing: Easing.ease }))
                 }
-                position.value = 0
-            }, 350)
+            }, 250)
         })
 
 
